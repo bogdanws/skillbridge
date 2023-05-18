5095f7707672da067762d6f6079c9361be66683a
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2023-05-18 17:20:42 +0300

    Fix Button types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-export default function Button({children, fill, className}: {children: string, fill?: boolean, className?: string}) {
+export default function Button({children, fill, className}: {children: string|React.ReactNode, fill?: boolean, className?: string}) {
     const style = fill ? "bg-primary text-white border-0" : "bg-white text-primary border-4 border-primary";
 
     return <button className={`p-3 rounded-md m-2 ${style} ${className}`}>
