a56d5fbd9ce122cd13c4cb2da8977a95484f5673
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2023-05-20 23:51:10 +0300

    Make default Button component client-side

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,16 @@
-export default function Button({children, fill, className}: {children: string|React.ReactNode, fill?: boolean, className?: string}) {
+"use client";
+
+import {MouseEventHandler} from "react";
+
+export default function Button({ children, fill, className, onClick }: {
+    children: string | React.ReactNode,
+    fill?: boolean,
+    className?: string,
+    onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+}) {
     const style = fill ? "bg-primary text-white border-0" : "bg-white text-primary border-4 border-primary";
 
-    return <button className={`p-3 rounded-md m-2 ${style} ${className}`}>
+    return <button className={`p-3 rounded-md m-2 ${style} ${className}`} onClick={onClick}>
         {children}
     </button>
 }
