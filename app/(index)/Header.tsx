61094c81cc1e275c80bd13e14d2e3c1826a753b2
GabrielaTudorache <125158248+GabrielaTudorache@users.noreply.github.com>
2023-05-20 21:48:09 +0300

    update header

--- app/(index)/Header.tsx
+++ app/(index)/Header.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 import header_photo from "@/public/Header_bg.svg";
 
 export default function Header() {
-    return <div className={'relative h-[30vh] border-2 border-gray m-4'}>
+    return <div className={'relative h-[30vh] md:h-[90vh] border-2 border-gray m-4'}>
         <Image src={header_photo} alt="" className={"opacity-50 blur-sm -scale-x-100 w-auto h-full absolute bottom-0 right-0 object-bottom object-contain"} />
-        <div className={'absolute flex flex-col justify-center items-center top-4 left-0 right-0 pt-1 mx-1'}>
-            <h1 className={'font-bold text-2xl md:'}>Learn everything from experts</h1>
-            <p>Get access to 1-on-1 lessons on your favorite subjects</p>
-            <div className={'flex'}>
-                <Button fill className={'shadow-lg'}>Get Started</Button>
-                <Button className={'hidden md:block'}>Learn More</Button>
+        <div className={'absolute flex flex-col justify-center items-center top-4 left-0 right-0 bottom-0 pt-1 mx-1 '}>
+            <h1 className={'font-bold text-2xl md:text-5xl '}>Learn everything from experts</h1>
+            <p className={'md:text-4xl'}>Get access to 1-on-1 lessons on your favorite subjects</p>
+            <div className={'flex md:m-10 '}>
+                <Button fill className={'shadow-lg md:p-5 md:m-10 md:text-2xl'}>Get Started</Button>
+                <Button className={'hidden md:block md:p-5 md:m-10 md:text-2xl'}>Learn More</Button>
             </div>
         </div>
     </div>
