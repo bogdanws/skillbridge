1d0645b917913e59c2ad4e2e12882f03d6ef0326
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2023-05-21 17:53:29 +0300

    Add tailwindcss/forms & /container-queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,25 @@ module.exports = {
 				'screen': '100dvh',
 			},
 			colors: {
-				primary: '#0D8C89',
-				secondary: '#003542',
+				primary: {
+					DEFAULT: "#0D8C89",
+					50: "#DEFCFB",
+					100: "#B9F8F7",
+					200: "#44EEEB",
+					300: "#14D2CF",
+					400: "#11B6B3",
+					500: "#0D8C89",
+					600: "#0C7E7C",
+					700: "#0A6B6A",
+					800: "#085957",
+					900: "#064140",
+					950: "#042A29"
+				},
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/container-queries'),
+	],
 }
