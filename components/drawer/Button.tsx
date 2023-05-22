35755d3893fd5152338015cdb7a5fcc526653908
GabrielaTudorache <125158248+GabrielaTudorache@users.noreply.github.com>
2023-05-22 22:25:27 +0300

    Dashboard drawer

--- components/drawer/Button.tsx
+++ components/drawer/Button.tsx
@@ -1,3 +1,8 @@
 export default function Button({children, label}: {children:React.ReactNode, label?: string}) {
-    return <button className={'hover:bg-gray text-primary-950 p-3'} aria-label={label}>{children}</button>;
+    return <button className={'hover:bg-gray text-neutral-700 p-4 max-md:py-0.5 flex flex-col justify-center items-center'} aria-label={label}>
+        {children}
+      <span className={'font-medium'}>
+        {label}
+      </span>
+    </button>;
 }
--- components/drawer/Drawer.tsx
+++ components/drawer/Drawer.tsx
@@ -1,22 +1,20 @@
 'use client';
 import Button from "@/components/drawer/Button";
-import {RxHamburgerMenu} from "react-icons/rx";
 import {ImHome} from "react-icons/im";
 import {FiSearch} from "react-icons/fi";
 import {BsChatLeftDotsFill, BsFillPersonFill} from "react-icons/bs";
 
+const iconSize = '1.5rem';
+
 export default function Drawer() {
     return <>
-        <div className={'w-40'}/>
+        <div className={'h-16 md:w-16'}/>
         <div
-            className={'w-40 h-full z-10 fixed left-0 flex flex-col justify-start items-stretch border-r-2 border-primary-950 bg-white rounded'}>
-            <Button label="Menu"><RxHamburgerMenu className={'w-[1.5em] h-[1.5em]'}/></Button>
-            <div className={'flex flex-col items-start'}>
-                <Button label="Home"><ImHome className={'w-[1.5em] h-[1.5em]'}/> Home</Button>
-                <Button label="Search"><FiSearch className={'w-[1.5em] h-[1.5em]'}/> Search</Button>
-                <Button label="Chat"><BsChatLeftDotsFill className={'w-[1.5em] h-[1.5em]'}/> Chat</Button>
-                <Button label="Profile"><BsFillPersonFill className={'w-[1.5em] h-[1.5em]'}/> Profile</Button>
-            </div>
+            className={'h-16 md:h-full w-full md:w-16 z-10 fixed bottom-0 md:left-0 flex flex-row md:flex-col justify-center border-t-2 md:border-r-2 border-primary-950 bg-white rounded'}>
+                <Button label="Home"><ImHome size={iconSize}/></Button>
+                <Button label="Search"><FiSearch size={iconSize}/></Button>
+                <Button label="Chat"><BsChatLeftDotsFill size={iconSize}/></Button>
+                <Button label="Profile"><BsFillPersonFill size={iconSize}/></Button>
         </div>
     </>
 }
