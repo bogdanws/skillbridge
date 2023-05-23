ddb83c05af0c779aa931d50f1f8aca6538f9cb13
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2023-05-23 22:17:22 +0300

    Change custom Button component to use twMerge

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,21 @@
 "use client";
+import {twMerge} from "tailwind-merge";
 
-export default function Button({children, fill, className, onClick, ...props}: {
-	children: string | React.ReactNode,
-	fill?: boolean,
-	className?: string,
-	onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined,
-	[key: string]: any
-}) {
-	const style = fill ? "bg-primary hover:bg-primary-600 text-white border-0"
+export default function Button({children, fill, className, onClick, ...props}: ButtonProps) {
+	// Get style based on fill
+	let style = fill ? "bg-primary hover:bg-primary-600 text-white border-0"
     : "bg-white hover:bg-neutral-100 text-primary border-4 border-primary";
 
-	return <button className={`transition-colors p-3 rounded-md m-2 ${style} ${className}`} onClick={onClick} {...props}>
+	style = twMerge("transition-colors p-3 rounded-md m-2", style, className);
+
+	return <button className={style} onClick={onClick} {...props}>
 		{children}
 	</button>
+}
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: string | React.ReactNode;
+	fill?: boolean;
+	className?: string;
+	onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
