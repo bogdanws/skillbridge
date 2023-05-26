ff24bb9558a62928a1629933dc363eadc5088bb5
GabrielaTudorache <125158248+GabrielaTudorache@users.noreply.github.com>
2023-05-26 20:12:14 +0300

    Create mobile view for chat

--- app/(dashboard)/chat/PeopleList.tsx
+++ app/(dashboard)/chat/PeopleList.tsx
@@ -1,11 +1,31 @@
+'use client';
 import PersonButton from './PersonButton';
-import {Chat} from "@prisma/client";
 import {ChatType} from "@/lib/twilio/chat";
+import {twMerge} from "tailwind-merge";
+import {useRouter} from "next/navigation";
 
-export default function PeopleList({chats} : {chats: ChatType[]}) {
-	return <div className={'w-80 h-full bg-primary-700 flex flex-col'}>
+export default function PeopleList({chats, className} : {chats: ChatType[], className?: string}) {
+	const classes = twMerge('w-80 h-full bg-primary-700 flex flex-col', className);
+
+	return <div className={classes}>
 		{chats.map(chat => {
 			return <PersonButton key={chat.id} chatName={chat.participants[0].name ?? chat.participants[0].email ?? "New User"} chatId={chat.id}/>
 		})}
+	</div>
+}
+
+export function PeopleListMobile({chats, className} : {chats: ChatType[], className?: string}) {
+	const router = useRouter();
+	const classes = twMerge('flex flex-col', className);
+
+	return <div className={classes}>
+		<select className={'w-full bg-primary-700 text-white p-4 rounded-lg'} onChange={(e) => {
+			router.push('/chat/' + e.target.value);
+		}}>
+			{chats.map(chat => {
+				return <option key={chat.id} value={chat.id}>{chat.participants[0].name ?? chat.participants[0].email ?? "New User"}
+				</option>
+			})}
+		</select>
 	</div>
 }
--- app/(dashboard)/chat/layout.tsx
+++ app/(dashboard)/chat/layout.tsx
@@ -1,4 +1,4 @@
-import PeopleList from './PeopleList';
+import PeopleList, {PeopleListMobile} from './PeopleList';
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/authOptions";
 import {getChats} from "@/lib/twilio/chat";
@@ -7,8 +7,9 @@ export default async function ChatLayout({children} : {children: React.ReactNode
 	const session = (await getServerSession(authOptions))!;
 	const chats = await getChats(session.user.id);
 
-	return <div className={'h-full flex flex-row'}>
-		<PeopleList chats={chats}/>
+	return <div className={'h-full flex flex-col md:flex-row'}>
+		<PeopleList chats={chats} className={'max-md:hidden'}/>
+		<PeopleListMobile chats={chats} className={'md:hidden'}/>
 		{children}
 	</div>
 }
