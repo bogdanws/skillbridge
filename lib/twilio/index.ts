d0e4f9179f22f0dd27d7ff6eb8fc8bdceedc3250
Bogdan Andreiana <bogdanandreiana13@gmail.com>
2023-05-26 20:52:41 +0300

    Fix Twilio chat

--- lib/twilio/index.ts
+++ lib/twilio/index.ts
@@ -1,4 +1,4 @@
 import twilio from "twilio";
 
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_API_KEY_SECRET);
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 export default twilioClient;
